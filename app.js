98d98e68188e91c5da9e51255fd9ac54cad295db
Alyson Dorfman <alyson@radiusclinic.com>
2023-09-25 16:36:14 -0700

    more javascript elements for fun

--- app.js
+++ app.js
@@ -3,7 +3,18 @@
        // console.log("I am in the HTML"); 
        alert("Welcome to my page!");
 
-       const usersName = prompt("Hey there! What's your name?");
+       const dogFan = prompt("Are you a fan of dogs?");
+       console.log("User says " + dogFan + " to if they're a fan of dogs.");
+
+       if (dogFan == "Yes"){
+        alert ("Good choice. We like you already!")
+       } else if (dogFan == "yes"){
+        alert ("Good choice. We like you already!")
+       } else {
+        alert ("Can't agree with you there, but let's move on, shall we?")
+       }
+
+       const usersName = prompt("By the way, what's your name?");
        console.log("User says their name is " + usersName);
 
        const usersDogtype = prompt("Would you say you prefer 1) classic, 2) wiener, or 3) long haired dogs? Please answer with a 1, 2, or 3.");
@@ -24,7 +35,7 @@
        } else if (usersName == "Adam Owada"){
         document.write(" YOU DA BOMB!");
        } else {
-        alert(" Glad to have you here!");
+        alert("Fantastic, we're glad to have you here!");
        }
 
 
