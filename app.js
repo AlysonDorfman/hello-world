12bee68d41209c1c21dc1b88042d5675562a167f
Alyson Dorfman <alyson@radiusclinic.com>
2023-09-25 14:44:39 -0700

    added more javascript into HTML to increase number of dynamic elements, and I added a joke for the TAs and professor

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+
        console.log("I am in the HTML"); 
        alert("Welcome to my page!");
 
@@ -10,15 +11,17 @@
        // if (this is true) {execute this code}
 
        if (usersName == "Kassie"){
-        alert("Hiya teach!");
+        document.write(" Hiya teach!");
        } else if (usersName == "Adam"){
-        alert("YOU DA BOMB!");
+        document.write(" YOU DA BOMB!");
        } else if (usersName == "Cameron"){
-        alert("YOU DA BOMB!");
+        document.write(" YOU DA BOMB!");
        } else if (usersName == "Cameron Walden"){
-        alert("YOU DA BOMB!");
-       } else if (usersName == "Adam Owada")
-       else {
-        alert("Glad to have you here!");
+        document.write(" YOU DA BOMB!");
+       } else if (usersName == "Adam Owada"){
+        document.write(" YOU DA BOMB!");
+       } else {
+        alert(" Glad to have you here!");
        }
+
 
