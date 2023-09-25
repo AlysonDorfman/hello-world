6d7363bbc0db22e8171f1050070c7202203b7b87
Alyson Dorfman <alyson@radiusclinic.com>
2023-09-25 14:22:17 -0700

    JavaScript Lecutre Follow Along

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+       console.log("I am in the HTML"); 
+       alert("Welcome to my page!");
+
+       const usersName = prompt("Hey there! What's your name?");
+       console.log(usersName);
+
+       document.write("Hi " + usersName + ", welcome to my page!")
+
+       // if (this is true) {execute this code}
+
+       if (usersName == "Kassie"){
+        alert("Hiya teach!");
+       } else if (usersName == "Adam"){
+        alert("YOU DA BOMB!");
+       } else if (usersName == "Cameron"){
+        alert("YOU DA BOMB!");
+       } else if (usersName == "Cameron Walden"){
+        alert("YOU DA BOMB!");
+       } else if (usersName == "Adam Owada")
+       else {
+        alert("Glad to have you here!");
+       }
+
