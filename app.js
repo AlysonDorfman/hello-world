4c1704f11471c9a596f066490073451a2aa7b198
Alyson Dorfman <alyson@radiusclinic.com>
2023-09-27 12:28:50 -0700

    adding functions to js

--- app.js
+++ app.js
@@ -3,24 +3,47 @@
        // console.log("I am in the HTML"); 
        alert("Welcome to my page!");
 
-       const dogFan = prompt("Are you a fan of dogs?");
-       console.log("User says " + dogFan + " to if they're a fan of dogs.");
+       // I put this in the HTML
+              //let usersName = getName();
+       let usersDogtype = getUsersDogtype();
+       let dogFan = getDogFan();
+       
 
-       if (dogFan == "Yes"){
-        alert ("Good choice. We like you already!")
-       } else if (dogFan == "yes"){
-        alert ("Good choice. We like you already!")
-       } else {
-        alert ("Can't agree with you there, but let's move on, shall we?")
+       function getName(){
+              const theirName = prompt("By the way, what's your name?");
+              console.log("User says their name is " + theirName);
+              return theirName;
        }
 
-       const usersName = prompt("By the way, what's your name?");
-       console.log("User says their name is " + usersName);
+       function greetUserInDoc(){
+              document.write("Arf! " + usersName + ", welcome to my page!")
+       }
 
-       const usersDogtype = prompt("Would you say you prefer 1) classic, 2) wiener, or 3) long haired dogs? Please answer with a 1, 2, or 3.");
-       console.log("User says their favorite dog type is number " + usersDogtype);
+       function getDogFan(){
+              const theirDogFan = prompt("Are you a fan of dogs?");
+              console.log("User says " + theirDogFan + " to if they're a fan of dogs.");
+              if (theirDogFan == "Yes"){
+                     alert ("Good choice. We like you already!")
+                     } else if (theirDogFan == "yes"){
+                     alert ("Good choice. We like you already!")
+                     } else if (theirDogFan == "yep"){
+                     alert ("Good choice. We like you already!")
+                     } else {
+                     alert ("Can't agree with you there, but let's move on, shall we?")
+                     }
+              return theirDogFan;
+       }
 
-       document.write("Arf! " + usersName + ", welcome to my page!")
+       function getUsersDogtype(){
+              const theirDogtype = prompt("Would you say you prefer 1) classic, 2) wiener, or 3) long haired dogs? Please answer with a 1, 2, or 3.");
+              console.log("User says their favorite dog type is number " + theirDogtype);
+              return theirDogtype;
+       }
+
+       
+
+     
+
 
        // if (this is true) {execute this code}
 
@@ -37,5 +60,20 @@
        } else {
         alert("Fantastic, we're glad to have you here!");
        }
+
+       // FUNCTIONS!!!
+
+       // function addTwoNumbers(num1, num2) {
+       //       let sum = num1 + num2;
+       //       console.log("Sum is: " + sum);
+       //       return sum;
+       
+
+       // let myNumber = addTwoNumbers();
+
+       // // invoking it, or calling it
+       // addTwoNumbers(1,2);
+       // addTwoNumbers(34,34); 
+
 
 
